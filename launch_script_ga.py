d7418cf60ef4cbc965ebe3f0acbbd60376cf4331
esaid <emmanuel.said@cern.ch>
2024-10-27 22:38:00 +0100

    ajout launch_script_gaparser

---
+++ launch_script_ga.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+
+# Afficher le répertoire courant (pour le débogage)
+# print("Le répertoire courant est :", os.getcwd())
+# Récupérer les arguments à passer au script
+args = sys.argv[1:]  # Arguments fournis à run_script.py
+
+# Déterminer le système d'exploitation
+if os.name == 'nt':  # Windows
+    script = os.path.join( 'ga144_script', 'windows', 'ga.exe')
+    [arg.replace('/', '\\') for arg in args]  # Remplacer / par \ pour Windows
+if os.name == 'posix':  # Linux ou autre
+    script = os.path.join('ga144_script','linux', './ga')
+    [arg.replace('\\', '/') for arg in args]   # Remplacer \ par / pour Linux
+
+
+# Afficher le script et les arguments (pour le débogage)
+print(f"Exécution de : {script} avec les arguments : {args}")
+
+# Exécuter le script avec les arguments
+subprocess.run('python ' + [script] + args)
