bce169ecc963ff5b6c4edc79ecdf35f25a153601
esaid <emmanuel.said@cern.ch>
2024-11-21 19:02:59 +0100

    scriptPath

--- src/extension.js
+++ src/extension.js
@@ -107,12 +107,14 @@ function activate(context) {
     let disposableSend = vscode.commands.registerCommand('myExtension.Ga144_Send', async function () {
         // vscode.commands.executeCommand('workbench.action.tasks.runTask', 'GA144 Send');
         const port = vscode.workspace.getConfiguration().get('myExtension.MyConfigurationSerialPort');
+        // le chemin absolu du script
+        const scriptPath = path.join(extensionPath, 'launch_send_script.py');
         const send_task = new vscode.Task(
             { type: 'shell' },
             vscode.TaskScope.Workspace,
             'GA144 Send Programming',
             'customTask',
-            new vscode.ShellExecution('python', ['${extensionPath}/launch_send_script.py', '${cwd}/${fileBasenameNoExtension}_.ga', '--port', port])
+            new vscode.ShellExecution('python', [scriptPath, '${cwd}/example/${fileBasenameNoExtension}_.ga', '--port', port])
         ); // script to execute from extensionPath and file example from current folder
         vscode.tasks.executeTask(send_task);
         vscode.window.showInformationMessage('Send --port ' + port);
